c49d3ece2af2ffb35457b72b5f5728552e1c4601
AntonyCandiotti <antonycandiotti123@hotmail.com>
2024-12-19 00:28:14 -0500

    Cambiando funcion de ping

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -1,16 +1,19 @@
 import {pool} from '../db.js';
 
 export const pong = async (req,res)=> {
-    const [result] = await pool.query(`CREATE TABLE users (
+
+    const querys = `CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
+    );
 
-INSERT INTO users (name, email) VALUES
-('Alice', 'alice@example.com'),
-('Bob', 'bob@example.com');`)
+    INSERT INTO users (name, email) VALUES
+    ('Alice', 'alice@example.com'),
+    ('Bob', 'bob@example.com');` 
+    const [result] = await pool.query(querys
+    )
     res.json(result)
 } 
 
